48d987741c8c7501516b24d8afd05fd27a92dd84
Your Name <your-email@example.com>
2025-11-29 18:58:05 +0530

    app check

--- src/app.js
+++ src/app.js
@@ -18,7 +18,7 @@ const PORT = Envs.PORT || 8010;
 app.use(express.json({ limit: "10MB" }));
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({
-    origin: ["http://localhost:5173", Envs.FRONT_END],
+    origin: Envs.FRONT_END || "https://livechatfrontend.onrender.com",
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
     allowedHeaders: ['Content-Type', 'Authorization'],
