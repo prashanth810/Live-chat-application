f9f653d53e365d0a6adc3be75efa782a704b09d2
Your Name <your-email@example.com>
2025-11-29 15:26:08 +0530

    cahnge from backend for deployemnnt changes

--- src/config/cloundnary/Multer.js
+++ src/config/cloundnary/Multer.js
@@ -1,15 +1,8 @@
 import multer from "multer";
-import { CloudinaryStorage } from "multer-storage-cloudinary";
-import cloudnary from "./Cloudinary.js";
 
-const storage = new CloudinaryStorage({
-    cloudinary: cloudnary, // REQUIRED property
-    params: {
-        folder: "chat_media",
-        resource_type: "auto",
-    },
+// Save uploaded files temporarily in /uploads folder
+const upload = multer({
+    dest: "chat_media",
 });
-
-const upload = multer({ storage });
 
 export default upload;
